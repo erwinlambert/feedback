126dca7a8375eea38a5cf5ed22f9b85796b0af46
erwinlambert <45197956+erwinlambert@users.noreply.github.com>
2021-09-14 15:00:31 +0200

    Read larmip data, include linear and quadratic option

--- src/utils.py
+++ src/utils.py
@@ -32,11 +32,27 @@ scol['245'] = (247/255,148/255,32/255)
 scol['370'] = (231/255,29/255,37/255)
 scol['585'] = (149/255,27/255,30/255)
 
-def oce2ice(TMP,IRF,bm=11.5):
+
+spy = 3600*24*365.25 #Seconds per year
+
+gamma = {} #Gamma value [m/s] from Favier et al 2019
+gamma['lin'] = 2e-5
+gamma['quad'] = 36e-5
+bmpexp = {} #Exponent in basal melt equation
+bmpexp['lin'] = 1
+bmpexp['quad'] = 2
+
+K = 1028*3947/(917*3.34e5)
+
+def basalmelt(T,bmp='lin',Tf=-1.7):
+    return gamma[bmp]*spy*(K*(T-Tf))**bmpexp[bmp]
+
+
+def oce2ice(TMP,IRF,bmp='lin'):
     #Input:
     #TMP: 5 temperature time series
     #IRF: Ice response function
-    #bm: basal melt sensitivity [m/yr /degC]
+    #bmp: basal melt parametrisation
     #Output:
     #IML: Ice mass loss for 5 regions [Gt/yr]
     assert TMP.shape == IRF.shape
@@ -44,12 +60,12 @@ def oce2ice(TMP,IRF,bm=11.5):
     for t,tt in enumerate(IRF.rftime):
         if t==0: continue
         for b,bas in enumerate(IRF.basin):
-            dFdt = TMP[1:t,b]-TMP[:t-1,b]
+            dFdt = basalmelt(TMP[1:t,b],bmp=bmp)-basalmelt(TMP[:t-1,b],bmp=bmp)
             CRF = IRF[1:t,b].values
-            IML[t,b] = bm*np.sum(CRF[::-1]*dFdt)
+            IML[t,b] = np.sum(CRF[::-1]*dFdt)
     return IML
 
-def oce2slr(TMP,SRF,bm=11.5):
+def oce2slr(TMP,SRF,bmp='lin'):
     #Input:
     #TMP: 5 temperature time series
     #SRF: Sealevel response function
@@ -61,9 +77,9 @@ def oce2slr(TMP,SRF,bm=11.5):
     for t,tt in enumerate(SRF.rftime):
         if t==0: continue
         for b,bas in enumerate(SRF.basin):
-            dFdt = TMP[1:t,b]-TMP[:t-1,b]
+            dFdt = basalmelt(TMP[1:t,b],bmp=bmp)-basalmelt(TMP[:t-1,b],bmp=bmp)
             CRF = SRF[1:t,b].values
-            SLR[t,b] = bm*np.sum(CRF[::-1]*dFdt)
+            SLR[t,b] = np.sum(CRF[::-1]*dFdt)
     return np.cumsum(SLR,axis=0)
 
 def ice2oce(IML,ORF):
@@ -84,15 +100,15 @@ def ice2oce(IML,ORF):
                 TMP[t,b] += np.sum(CRF[::-1]*dFdt)
     return TMP
 
-def iterate(ds,ism,esm,ssp,niter=4):
+def iterate(ds,ism,esm,ssp,bmp='lin',niter=4):
     TMP = np.zeros((niter,len(ds.time),len(ds.basin)))
     IML = np.zeros((niter,len(ds.time),len(ds.basin)))
     SLR = np.zeros((niter,len(ds.time),len(ds.basin)))
     TMP[0,:,:] = ds.temp.sel(esm=esm,ssp=ssp).values
-    IML[0,:,:] = oce2ice(TMP[0,:,:],ds.irf.sel(ism=ism))
-    SLR[0,:,:] = oce2slr(TMP[0,:,:],ds.srf.sel(ism=ism))
+    IML[0,:,:] = oce2ice(TMP[0,:,:],ds.irf.sel(ism=ism),bmp=bmp)
+    SLR[0,:,:] = oce2slr(TMP[0,:,:],ds.srf.sel(ism=ism),bmp=bmp)
     for n in range(1,niter):
         TMP[n,:,:] = ds.temp.sel(esm=esm,ssp=ssp).values + ice2oce(IML[n-1,:,:],ds.orf)
-        IML[n,:,:] = oce2ice(TMP[n,:,:],ds.irf.sel(ism=ism))
-        SLR[n,:,:] = oce2slr(TMP[n,:,:],ds.srf.sel(ism=ism))
+        IML[n,:,:] = oce2ice(TMP[n,:,:],ds.irf.sel(ism=ism),bmp=bmp)
+        SLR[n,:,:] = oce2slr(TMP[n,:,:],ds.srf.sel(ism=ism),bmp=bmp)
     return TMP,IML,SLR
