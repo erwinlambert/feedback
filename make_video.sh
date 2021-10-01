95e8072f5b3760b4da8a7c1cac449a21e183ee67
erwinlambert <erwin.lambert@knmi.nl>
2021-10-01 13:29:51 +0000

    sections for all regions, code to make video

---
+++ make_video.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+echo -n "Give prefix: "
+read -r prefix
+
+ffmpeg -r 4 -f image2 -s 1920x1080 -i videos/${prefix}_%03d.png -vcodec libx264 -crf 25  -pix_fmt yuv420p videos/${prefix}.mp4
