f3f7a2cd8f2b217da8c5d0585a154fe83edb7055
erwinlambert <erwin.lambert@knmi.nl>
2021-10-04 13:55:21 +0000

    proper Arctic videos

--- make_video.sh
+++ make_video.sh
@@ -4,3 +4,4 @@ echo -n "Give prefix: "
 read -r prefix
 
 ffmpeg -r 4 -f image2 -s 1920x1080 -i videos/${prefix}_%03d.png -vcodec libx264 -crf 25  -pix_fmt yuv420p videos/${prefix}.mp4
+rm -r videos/${prefix}*.png
