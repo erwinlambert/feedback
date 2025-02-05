cad1918ce2a5b9ffb7126acc415f2ac7235913a6
erwinlambert <erwin.lambert@knmi.nl>
2025-02-05 11:04:50 +0000

    Get sections back

---
+++ src/utils.py
@@ -0,0 +1,11 @@
+import numpy as np
+import matplotlib as mpl
+
+basin = ['East Ant.','Ross','Amundsen','Weddell','Peninsula']
+
+bcol = {}
+bcol['East Ant.'] = 'tab:blue'
+bcol['Ross']      = 'tab:orange'
+bcol['Amundsen']  = 'tab:red'
+bcol['Weddell']   = 'tab:purple'
+bcol['Peninsula'] = 'tab:green'
